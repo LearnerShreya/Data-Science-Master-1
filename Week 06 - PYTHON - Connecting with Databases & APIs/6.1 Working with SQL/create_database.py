4d095ffe612a1325d2b4735fe1fd9cb5da6dc968
Shreya Singh <shreya.official.2.25@gmail.com>
2025-01-30 19:11:25 +0530

    delete all

--- Week 06 - PYTHON - Connecting with Databases & APIs/6.1 Working with SQL/create_database.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.connect( 
-    host = 'localhost',
-    user='root',
-    password='0618' 
-)
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE if not exists test2")
-mydb.close()
--- Week 06 - PYTHON - Connecting with Databases & APIs/6.1 Working with SQL/create_table.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.connect( 
-    host = 'localhost',
-    user='root',
-    password='0618' 
-)
-mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE if not exists test2.test_table(c1 INT, c2 VARCHAR(50), c3 FLOAT, c4 INT, c5 VARCHAR(30));")
-mydb.close()
